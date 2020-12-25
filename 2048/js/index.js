a29be8b5c25c6d0e1efe6b094c7d67e7f981e593
jiangjie1105 <1021404964@qq.com>
2020-12-25 22:54:44 +0800

    2048

---
+++ 2048/js/index.js
@@ -0,0 +1,14 @@
+// 用二维数组存储数据
+var data = [[0, 0, 0, 0,], [0, 0, 0, 0,], [0, 0, 0, 0,], [0, 0, 0, 0,]];
+// 总行数
+var rn = 4;
+// 总列数
+var cn = 4;
+// 游戏状态
+var state = 0; //running || gameover 
+// 游戏进行中
+var running = 1;
+// 游戏结束
+var gameover = 0;
+// 分数
+var score = 0;
